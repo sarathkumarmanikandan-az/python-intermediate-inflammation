33084195cfa63c181d68f8e3aebc4de7e200e434
Sarathkumar Manikandan <sarathkumar.manikandan@astrazeneca.com>
2023-05-16 20:19:06 +0530

    code style fixed in main function

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -2,7 +2,6 @@
 """Software for managing and analysing patients' inflammation data in our imaginary hospital."""
 
 import argparse
-
 from inflammation import models, views
 
 
@@ -17,13 +16,16 @@ def main(args):
     if not isinstance(InFiles, list):
         InFiles = [args.infiles]
 
-
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {'average': models.daily_mean(inflammation_data),
+                     'max': models.daily_max(inflammation_data),
+                     'min': models.daily_min(inflammation_data)
+        }
 
         views.visualize(view_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
